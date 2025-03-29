097dd54e3e4471920df2495765ffb1d6f535fb2b
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2025-03-29 14:47:44 +0530

    Create Sales_lookup.py

---
+++ Sales_lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+### import mongoSettings.py and other required libraries
+from mongoSettings import *
+import pymongo
+import sys
+import random
+import uuid
+import math
+import string
